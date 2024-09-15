cc6b3fed0f0c7ee99a5a400590536dcad165a5b0
Contreras Perales Rodrigo <levicom134@gmail.com>
2024-09-15 10:32:33 -0500

    Forma de carpetas done

--- app/src/main/java/com/kotlin/integrador/ui/register/register.kt
+++ app/src/main/java/com/kotlin/integrador/ui/register/register.kt
@@ -1,16 +1,17 @@
-package com.kotlin.integrador
+package com.kotlin.integrador.ui.register
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.kotlin.integrador.R
 
-class MainActivity : AppCompatActivity() {
+class register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
