1b69babc6e07db70334d92f0b7025ce5620e69b0
Contreras Perales Rodrigo <levicom134@gmail.com>
2024-10-20 20:20:00 -0500

    Create new Activity

---
+++ app/src/main/java/com/kotlin/integrador/ui/newendpoint/NewEndpoint.kt
@@ -0,0 +1,21 @@
+package com.kotlin.integrador.ui.newendpoint
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.kotlin.integrador.R
+
+class NewEndpoint : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_new_endpoint)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
